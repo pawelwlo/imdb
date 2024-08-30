3d9633cb054a0339714145a3ad579b10814132ef
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-08-30 16:31:43 +0200

    new design

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ const resetButton = document.getElementById("reset-button")
 
 // localStorage.clear()
 
-if (resetButton) {
-     resetButton.addEventListener("click", function(){
-          alert("your search results were cleared")
-          
-          window.localStorage.removeItem("movies");
-          location.reload(); 
-     })
-}
-
-
-
 let movies = [];
 let moviesFromLocalStorage = JSON.parse(localStorage.getItem("movies"));
 
@@ -24,6 +13,7 @@ if (moviesFromLocalStorage && moviesFromLocalStorage.length > 0) {
     movies = moviesFromLocalStorage;
     renderMovies();
 }
+
 if (searchButton) {
      searchButton.addEventListener("click", getFilms);
 }
@@ -42,39 +32,37 @@ async function getFilms(e) {
                
                 <img class="poster" src="${data['Poster']}">
                 <div class="movie-text">
-                    <p>Title: ${data['Title']}</p>
+                    <p class="title">${data['Title']}</p>
                     <p>Year: ${data['Year']}</p>
                     <p>Actors: ${data['Actors']}</p>
                     <p>${data['Runtime']}</p>
                     <p><img class="rating" src="star.png"> ${data['imdbRating']}</p>
                     <p>Genre: ${data['Genre']}</p>
                     <p>${data['Plot'].slice(0,500)}...</p>
-                    
+                    <button class="button add" id="button-wishList">+</button> <span id="add-button-text">Add to Watchlist</span>
+                    <button class="button delete" id="delete-button">-</button>
                 </div>
                 
                
                
             </div>
-            <button id="button-wishList">+</button> <a id="add-button-text">Add to Watchlist</a>
+            
            
-            <hr>
         `
+        
     };
 
-//     document.addEventListener("click", function(e) {
-//      if (e.target && e.target.classList.contains("button-wishList")) {
-//          addToWishList(e);
-         
-//      }
-//  });
+    
+    console.log(movie)
     movies.unshift(movie);
     localStorage.setItem("movies", JSON.stringify(movies));
+    
     renderMovies();
     searchInput.value = "";
     location.reload(); 
 }
 
-
+// console.log(moviesFromLocalStorage)
 
 function renderMovies() {
      if(resultOutput) {
@@ -83,61 +71,85 @@ function renderMovies() {
      }
    
 }
-
-let buttonWish = document.getElementById("button-wishList");
-let moviesWishList = []
-
-
-let movieToAddFromLocalStorage = JSON.parse(localStorage.getItem("moviesWishList"));
-
-if (movieToAddFromLocalStorage) {
-     moviesWishList = movieToAddFromLocalStorage;
-     renderMoviesWishList()
-}
+// const buttonWish = document.getElementById("button-wishList");
+// const deleteButton = document.getElementById("delete-button")
+// let moviesWishList = []
 
 // function addToWishList(e) {
 //      e.preventDefault();
-//      let movieElement = e.target.closest('.movie');
-//      let movieToAdd = movieElement.outerHTML;
+     
+//      let movieToAdd = e.target.parentElement.innerHTML;
 //      moviesWishList.unshift(movieToAdd);
 //      localStorage.setItem("moviesWishList", JSON.stringify(moviesWishList));
+//      console.log("movie added to wish list")
 //      renderMoviesWishList();
      
 //  }
 
-if(buttonWish) {
-     buttonWish.addEventListener("click", function(e) {
-          e.preventDefault();
-          console.log(e)
-          let movieToAdd = e.target.parentElement.innerHTML;
-          moviesWishList.unshift(movieToAdd);
-          localStorage.setItem("moviesWishList", JSON.stringify((moviesWishList) ))
-          alert("New movie was added to your watch list")
-         renderMoviesWishList()
-         
+//  if(buttonWish) {
+//      buttonWish.addEventListener("click", addToWishList)
+//  }
+
+
+// let movieToAddFromLocalStorage = JSON.parse(localStorage.getItem("moviesWishList"));
+
+// if (movieToAddFromLocalStorage) {
+//      moviesWishList = movieToAddFromLocalStorage;
+//      renderMoviesWishList()
+// }
+
+
+
+// if(buttonWish) {
+//      buttonWish.addEventListener("click", function(e) {
+//           e.preventDefault();
+//           console.log("button wish clicked")
+//           let movieToAdd = e.target.parentElement.innerHTML;
+//           moviesWishList.unshift(movieToAdd);
+//           localStorage.setItem("moviesWishList", JSON.stringify((moviesWishList) ))
+//           alert("New movie was added to your watch list")
+//          renderMoviesWishList()
+        
          
           
          
+//      })
+// }
+
+// if(deleteButton) {
+//      deleteButton.addEventListener("click", function(e) {
+//           e.preventDefault()
+//           const movieToDelete = e.target.parentElement.innerHTML;
+//           movieToAddFromLocalStorage.removeItem(movieToDelete)
+//           renderMoviesWishList()
+//           console.log("movie deleted from wishlist")
+//      }
+
+//      )
+// }
+
+// function renderMoviesWishList() {
+//      if (wishListDisplay) {
+//           wishListDisplay.innerHTML = moviesWishList;
+//      }
+     
+// }
+
+// document.addEventListener("click", function(e){
+//      console.log(e.target)
+// })
+
+
+
+
+// localStorage.setItem("moviesWishList", JSON.stringify(moviesWishList));
+
+if (resetButton) {
+     resetButton.addEventListener("click", function(){
+          alert("your search results were cleared")
+          
+          window.localStorage.removeItem("movies");
+          location.reload(); 
      })
 }
 
-
-
-function renderMoviesWishList() {
-     if (wishListDisplay) {
-          wishListDisplay.innerHTML = moviesWishList;
-     }
-     
-}
-
-document.addEventListener("click", function(e){
-     console.log(e.target)
-})
-
-const removeString = '<button id="button-wishList">+</button> <a id="add-button-text">Add to Watchlist</a>';
-let newWishList = moviesWishList.map(item=> item.replace(removeString, ''))
-
-moviesWishList = newWishList;
-localStorage.setItem("moviesWishList", JSON.stringify(moviesWishList));
-
-console.log(newWishList)
